f80b951370744432157d9b91c93949035ddcab69
alaysius <82561108+alaysius@users.noreply.github.com>
2021-10-10 21:13:45 -0700

    Add files via upload

---
+++ resources/assets/js/components/Products.vue
@@ -0,0 +1,259 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Products Table</h3>
+
+                <div class="card-tools">
+                    <button class="btn btn-success" @click="newModal">Create <i class="fas fa-user-plus fa-fw"></i></button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Product Name</th>
+                        <th>Category</th>
+                        <th>Description</th>
+                        <th>Registered At</th>
+                        <th>Modify</th>
+                  </tr>
+
+
+                  <tr v-for="user in users.data" :key="user.id">
+
+                    <td>{{user.id}}</td>
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.type | upText}}</td>
+                    <td>{{user.created_at | myDate}}</td>
+
+                    <td>
+                        <a href="#" @click="editModal(user)">
+                            <i class="fa fa-edit blue"></i>
+                        </a>
+                        /
+                        <a href="#" @click="deleteUser(user.id)">
+                            <i class="fa fa-trash red"></i>
+                        </a>
+
+                    </td>
+                  </tr>
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                  <pagination :data="users" @pagination-change-page="getResults"></pagination>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+
+       
+    <!-- Modal -->
+            <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" v-show="!editmode" id="addNewLabel">Add New</h5>
+                    <h5 class="modal-title" v-show="editmode" id="addNewLabel">Update Product's Info</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form @submit.prevent="editmode ? updateUser() : createUser()">
+                <div class="modal-body">
+                     <div class="form-group">
+                        <input v-model="form.name" type="text" name="name"
+                            placeholder="Name"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+
+                     <div class="form-group">
+                        <input v-model="form.email" type="email" name="email"
+                            placeholder="Email Address"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="email"></has-error>
+                    </div>
+
+                    
+                    <div class="form-group">
+                        <select name="type" v-model="form.type" id="category" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                            <option value="">Select Category</option>
+                            <option value="Computer Parts">Computer Parts</option>
+                            <option value="CCTV Cameras">CCTV</option>
+                            <option value="Peripherals">Peripherals</option>
+                        </select>
+                        <has-error :form="form" field="type"></has-error>
+                    </div>
+
+                     <div class="form-group">
+                        <textarea v-model="form.bio" name="description" id="description"
+                        placeholder="Description (Optional)"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                        <has-error :form="form" field="bio"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="photo" class="col-sm-2 control-label">Profile Photo</label>
+                        <div class="col-sm-12">
+                        <input type="file" @change="updateProfile" name="photo" class="form-input">
+                    </div>
+
+                    <div class="form-group">
+                        <input v-model="form.password" type="password" name="password" id="password"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                        <has-error :form="form" field="password"></has-error>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button v-show="editmode" type="submit" class="btn btn-success">Update</button>
+                    <button v-show="!editmode" type="submit" class="btn btn-primary">Create</button>
+                </div>
+
+                </form>
+
+                </div>
+            </div>
+            </div>
+    </div>
+
+
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                editmode: false,
+                users : {},
+                form: new Form({
+                    id:'',
+                    name : '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
+        methods: {
+            getResults(page = 1) {
+                        axios.get('api/user?page=' + page)
+                            .then(response => {
+                                this.users = response.data;
+                            });
+                },
+            updateUser(){
+                this.$Progress.start();
+                // console.log('Editing data');
+                this.form.put('api/user/'+this.form.id)
+                .then(() => {
+                    // success
+                    $('#addNew').modal('hide');
+                     swal(
+                        'Updated!',
+                        'Information has been updated.',
+                        'success'
+                        )
+                        this.$Progress.finish();
+                         Fire.$emit('AfterCreate');
+                })
+                .catch(() => {
+                    this.$Progress.fail();
+                });
+
+            },
+            editModal(user){
+                this.editmode = true;
+                this.form.reset();
+                $('#addNew').modal('show');
+                this.form.fill(user);
+            },
+            newModal(){
+                this.editmode = false;
+                this.form.reset();
+                $('#addNew').modal('show');
+            },
+            deleteUser(id){
+                swal({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+
+                        // Send request to the server
+                         if (result.value) {
+                                this.form.delete('api/user/'+id).then(()=>{
+                                        swal(
+                                        'Deleted!',
+                                        'Your file has been deleted.',
+                                        'success'
+                                        )
+                                    Fire.$emit('AfterCreate');
+                                }).catch(()=> {
+                                    swal("Failed!", "There was something wronge.", "warning");
+                                });
+                         }
+                    })
+            },
+           loadUsers(){
+                if(this.$gate.isUser()){
+                    axios.get("api/user").then(({ data }) => (this.users = data));
+                }
+            },
+
+            createUser(){
+                this.$Progress.start();
+
+                this.form.post('api/user')
+                .then(()=>{
+                    Fire.$emit('AfterCreate');
+                    $('#addNew').modal('hide')
+
+                    toast({
+                        type: 'success',
+                        title: 'User Created in successfully'
+                        })
+                    this.$Progress.finish();
+
+                })
+                .catch(()=>{
+
+                })
+            }
+        },
+        created() {
+            Fire.$on('searching',() => {
+                let query = this.$parent.search;
+                axios.get('api/findUser?q=' + query)
+                .then((data) => {
+                    this.users = data.data
+                })
+                .catch(() => {
+
+                })
+            })
+           this.loadUsers();
+           Fire.$on('AfterCreate',() => {
+               this.loadUsers();
+           });
+        //    setInterval(() => this.loadUsers(), 3000);
+        }
+
+    }
+</script>
