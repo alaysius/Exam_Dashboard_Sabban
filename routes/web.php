fd1b28d97effdfe860550c29525a7c1c65c3aa31
alaysius <82561108+alaysius@users.noreply.github.com>
2021-10-10 21:14:13 -0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('invoice', function(){
+    return view('invoice');
+});
+
+Route::get('{path}','HomeController@index')->where('path','([A-z\d-/_.]+)?');
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
